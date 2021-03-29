00df1cd4b59f0fb85d16981d8d12066d2467c933
yj0903 <wiseidea1@hanmail.net>
2021-03-29 16:20:30 +0900

    2021.03.29 복습

--- quiz/03-2/sample/js/app.js
+++ quiz/03-2/sample/js/app.js
@@ -1,12 +1,26 @@
 // 실습 #1 - `todo-footer` 컴포넌트 전역 등록
 // <p>This is another child global component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+    Vue.component('todo-footer', {
+      template: '<p>This is another child global component</p>'
+    })
 
-var app = new Vue({
-  el: '#app',
-  data: {
-    message : 'This is a parent component'
-  },
+// 실습 #2 - `todo-list` 컴포넌트 지역 등록
+// <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
 
-  // 실습 #2 - `todo-list` 컴포넌트 지역 등록
-  // <p>This is another child local component</p> 를 template 으로 갖는 컴포넌트를 등록해보세요.
+    // 지역컴포넌트의 내용
+    var cmp = {
+      template: '<p>This is another child local component</p>'
+    };
+
+    // 인스턴스 생성
+    var app = new Vue({
+      el: '#app',
+      data: {
+        message : 'This is a parent component'
+      },
+
+      // 인스턴스 속성 중 components를 이용해 지역 등록함.
+      components:{
+        'todo-list': cmp
+      }
 });
--- quiz/03-3/sample/js/app.js
+++ quiz/03-3/sample/js/app.js
@@ -1,19 +1,25 @@
-// 할일 #1
 // sibling-component 를 이름으로 갖는 새로운 컴포넌트를 아래에 등록해보세요.
 // options : template, props
 
+// 자식컴포넌트 1
 Vue.component('child-component', {
   props: ['propsdata'],
   template: '<p>{{ propsdata }}</p>'
 });
+// 자식 컴포넌트 2
+Vue.component('sibling-component', {
+  props: ['propsdata'],
+  template: '<p>{{ propsdata }}</p>'
+});
 
+
+// 이게 루트 컴포넌트 
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue! passed from Parent Component',
-
-    // 할일 #2
     // data 속성을 1개 더 지정하고 (예: anotherMessage) 임의의 문자열을 값으로 대입해보세요.
     // 새로 지정한 data 속성은 위 sibling-component에 props로 전달합니다.
+    anotherMessage: 'Hello Vue.js! passed from Parent Component'
   }
 });
